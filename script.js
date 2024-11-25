c7d850c2344dc3e1054956a67ba9b6d1d31317b0
webQbe <webcubesolutionslk@gmail.com>
2024-11-25 21:55:03 +0530

    Defined createList() to generate list items

--- script.js
+++ script.js
@@ -20,6 +20,34 @@ const richestPeople = [
 const listItems = []; // Store list items
 let dragStartIndex; // Keep track of each list item's index
 
+createList(); // Generate list items
+
+function createList(){
+    [...richestPeople] // Make a copy of the array without scrambling its order
+     .forEach((person, index) => {
+
+        // Create <li> element for current person
+        const listItem = document.createElement('li');
+
+        // Set current index as <li> element's data-index
+        listItem.setAttribute('data-index', index);
+
+        // Add index + 1 & person to <li> element 
+        listItem.innerHTML = `
+            <span class="number">${index + 1}</span>
+            <div class="draggable" draggable="true">
+                <p class="person-name">${person}</p>
+                <i class="fas fa-grip-lines"></i>
+            </div>`;
+
+        // Add current item to listItems array     
+        listItems.push(listItem);
+
+        // Append current item to draggable_list element
+        draggable_list.appendChild(listItem);
+
+     });
+}
 
 
 
