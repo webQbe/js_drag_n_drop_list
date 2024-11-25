f27f83f152dab70dac0588d02d7e65a1616048f5
webQbe <webcubesolutionslk@gmail.com>
2024-11-25 21:27:02 +0530

    Defined correctly ordered richestPeople array

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 // Define DOM Elements
 const draggable_list = document.getElementById('draggable-list');
 const checkBtn = document.getElementById('check'); 
+
+// Define correctly ordered array
+const richestPeople = [
+    'Elon Musk',
+    'Jeff Bezos',
+    'Larry Ellison ',
+    'Bernard Arnault & Family ',
+    'Mark Zuckerberg',
+    'Warren Buffett',
+    'Bill Gates',
+    'Larry Page ',
+    'Sergey Brin',
+    'Amancio Ortega'
+];
+
